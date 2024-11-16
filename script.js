c35f587fb779e27c40ce2c519073afc53a234bf1
Erik Nowlin <4080942+ejnowlin@users.noreply.github.com>
2024-11-16 12:01:04 -0800

    feat: Add listener to create new grid from user input

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const gridContainer = document.querySelector("#grid-container");
+const gridSizeBtn = document.querySelector("#grid-size-btn");
+var gridSquareSize = document.createElement("style");
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    gridContainer.appendChild(div);
-    div.classList.toggle("square");
+function addNewGrid(input) {
+    let newGridSize = 1;
+
+    if (parseInt(input) > 0 && parseInt(input) <= 100) {
+        newGridSize = parseInt(input);
+    } else {
+        alert("Invalid input. Click the button again and enter a number from 1-100.");
+    }
+
+    while (gridContainer.firstElementChild) {
+        gridContainer.removeChild(gridContainer.firstElementChild);
+    }
+
+    for (let i = 0; i < (newGridSize * newGridSize); i++) {
+        const div = document.createElement("div");
+        gridContainer.appendChild(div);
+        div.classList.toggle("square");
+    }
+
+    let divSide = gridContainer.clientWidth / newGridSize;
+    gridSquareSize.innerHTML = `
+        div.square {
+        width: ${divSide}px;
+        height: ${divSide}px;
+        }`;
+    document.head.appendChild(gridSquareSize);
 }
 
 gridContainer.addEventListener("mouseover", (event) => {
     if (event.target.className === "square")
         event.target.style.cssText = "background-color: grey;";
+});
+
+gridSizeBtn.addEventListener("click", () => {
+    let userInput = prompt("How many squares per side of the new grid?", "100 max");
+    addNewGrid(userInput);
 });
